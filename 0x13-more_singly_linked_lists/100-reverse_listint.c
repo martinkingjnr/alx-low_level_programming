81cd4f0fcae9b8089d71e076ecf37d219beb692a
martinkingjnr <kingjnr20.com@gmail.com>
2023-04-29 20:28:15 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,18 +8,17 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
- listint_t *prev = NULL;
- listint_t *next = NULL;
+listint_t *prev = NULL;
+listint_t *next = NULL;
 
- while (*head)
- {
- next = (*head)->next;
- (*head)->next = prev;
- prev = *head;
- *head = next;
- }
+while (*head)
+{
+next = (*head)->next;
+(*head)->next = prev;
+prev = *head;
+*head = next;
+}
 
- *head = prev;
-
- return (*head);
+*head = prev;
+return (*head);
 }
